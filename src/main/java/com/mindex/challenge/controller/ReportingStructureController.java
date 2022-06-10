66194eea006dcbe2c4fa07e7247d966d75f62d4b
theresa <tphan@paradigmtesting.com>
2022-06-09 20:27:11 -0400

    adding .gitignore file

---
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -0,0 +1,12 @@
+package com.mindex.challenge.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@RequestMapping
+public class ReportingStructureController {
+
+
+}
